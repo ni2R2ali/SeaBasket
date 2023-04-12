06b547fe9ef91c7b99ded271cd5117003afe1ec7
Nirali Tak <nirali.tak@seaflux.tech>
2023-04-13 03:37:49 +0530

    Adding confirmation to sidebar signout

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../Service/auth.service';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +10,7 @@ import { AuthService } from '../Service/auth.service';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-  constructor(public auth: AuthService){}
+  constructor(public auth: AuthService, private router: Router, public dialog: MatDialog){}
 
   opened = false;
 
@@ -17,6 +20,19 @@ export class SidebarComponent {
 
   signOut() {
     this.auth.logOut();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '310px',
+      data: 'Are you sure you want to log out?',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
 }
